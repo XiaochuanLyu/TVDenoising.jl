704b26343aa87ea67003e3086c76413d3096c8f4
XiaochuanLyu <lyuxiaochuan@gmail.com>
2026-06-22 14:12:22 -0400

    feat: add tv1D_condat_v2, build new checks in main, modified package address

--- src/1D_denoising.jl
+++ src/1D_denoising.jl
@@ -1,7 +1,7 @@
 using LinearAlgebra
 
-function tv1d_condat(y::Vector{Ty}, λ::U) where {Ty <: Number, U <: Real}
-    T = promote_type(Ty, Float64)
+function tv1d_condat_v1(y::AbstractVector{Ty}, λ::U) where {Ty <: Number, U <: Real}
+    T = promote_type(Ty, U, Float64)
     N = length(y)
     if N == 0
         return T[]
@@ -10,8 +10,9 @@ function tv1d_condat(y::Vector{Ty}, λ::U) where {Ty <: Number, U <: Real}
         throw(ArgumentError("λ must be nonnegative."))
     end
     if λ == 0
-        return y
+        return T.(y)
     end
+    λ = T(λ)
     x = Vector{T}(undef, N)
     # Step 1
     k = 1
@@ -121,4 +122,248 @@ function tv1d_condat(y::Vector{Ty}, λ::U) where {Ty <: Number, U <: Real}
             return x
         end
     end
+end
+
+function tv1d_condat_v2(y::AbstractVector{Ty}, λ::U) where {Ty <: Number, U <: Real}
+    T = promote_type(Ty, U, Float64)
+    N = length(y)
+    if N == 0
+        return T[]
+    end
+    if λ < 0
+        throw(ArgumentError("λ must be nonnegative."))
+    end
+    if λ == 0
+        return T.(y)
+    end
+    λ = T(λ)
+    x = Vector{T}(undef, N)
+    if N == 1
+        x[1] = T(y[1])
+        return x
+    end
+    indstart_low = Vector{Int}(undef, N)
+    indstart_up = Vector{Int}(undef, N)
+    j_low = 1
+    j_up = 1
+    jseg = 1
+    indjseg = 1
+    output_low_first = T(y[1]) - λ
+    output_low_curr = output_low_first
+    output_up_first = T(y[1]) + λ
+    output_up_curr = output_up_first
+    twoλ = 2 * λ
+    indstart_low[1] = 1
+    indstart_up[1] = 1
+
+    @inbounds for i in 2:(N - 1)
+        yi = T(y[i])
+        if yi >= output_low_curr
+            if yi <= output_up_curr
+                output_up_curr += (yi - output_up_curr) / (i - indstart_up[j_up] + 1)
+                x[indjseg] = output_up_first
+                while j_up > jseg
+                    ind = indstart_up[j_up - 1]
+                    if output_up_curr > x[ind]
+                        break
+                    end
+                    output_up_curr += (x[ind] - output_up_curr) *
+                                      ((indstart_up[j_up] - ind) / (i - ind + 1))
+                    j_up -= 1
+                end
+                if j_up == jseg
+                    while output_up_curr <= output_low_first && jseg < j_low
+                        jseg += 1
+                        indjseg2 = indstart_low[jseg]
+                        output_up_curr += (output_up_curr - output_low_first) *
+                                          ((indjseg2 - indjseg) / (i - indjseg2 + 1))
+                        while indjseg < indjseg2
+                            x[indjseg] = output_low_first
+                            indjseg += 1
+                        end
+                        output_low_first = x[indjseg]
+                    end
+                    output_up_first = output_up_curr
+                    j_up = jseg
+                    indstart_up[j_up] = indjseg
+                else
+                    x[indstart_up[j_up]] = output_up_curr
+                end
+            else
+                j_up += 1
+                indstart_up[j_up] = i
+                x[i] = yi
+                output_up_curr = yi
+            end
+            output_low_curr += (yi - output_low_curr) / (i - indstart_low[j_low] + 1)
+            x[indjseg] = output_low_first
+            while j_low > jseg
+                ind = indstart_low[j_low - 1]
+                if output_low_curr < x[ind]
+                    break
+                end
+                output_low_curr += (x[ind] - output_low_curr) *
+                                   ((indstart_low[j_low] - ind) / (i - ind + 1))
+                j_low -= 1
+            end
+            if j_low == jseg
+                while output_low_curr >= output_up_first && jseg < j_up
+                    jseg += 1
+                    indjseg2 = indstart_up[jseg]
+                    output_low_curr += (output_low_curr - output_up_first) *
+                                       ((indjseg2 - indjseg) / (i - indjseg2 + 1))
+                    while indjseg < indjseg2
+                        x[indjseg] = output_up_first
+                        indjseg += 1
+                    end
+                    output_up_first = x[indjseg]
+                end
+                j_low = jseg
+                indstart_low[j_low] = indjseg
+                if indjseg == i
+                    output_low_first = output_up_first - twoλ
+                else
+                    output_low_first = output_low_curr
+                end
+            else
+                x[indstart_low[j_low]] = output_low_curr
+            end
+        else
+            j_low += 1
+            indstart_low[j_low] = i
+            x[i] = yi
+            output_low_curr = yi
+            output_up_curr += (output_low_curr - output_up_curr) / (i - indstart_up[j_up] + 1)
+            x[indjseg] = output_up_first
+            while j_up > jseg
+                ind = indstart_up[j_up - 1]
+                if output_up_curr > x[ind]
+                    break
+                end
+                output_up_curr += (x[ind] - output_up_curr) *
+                                  ((indstart_up[j_up] - ind) / (i - ind + 1))
+                j_up -= 1
+            end
+            if j_up == jseg
+                while output_up_curr <= output_low_first && jseg < j_low
+                    jseg += 1
+                    indjseg2 = indstart_low[jseg]
+                    output_up_curr += (output_up_curr - output_low_first) *
+                                      ((indjseg2 - indjseg) / (i - indjseg2 + 1))
+                    while indjseg < indjseg2
+                        x[indjseg] = output_low_first
+                        indjseg += 1
+                    end
+                    output_low_first = x[indjseg]
+                end
+                j_up = jseg
+                indstart_up[j_up] = indjseg
+                if indjseg == i
+                    output_up_first = output_low_first + twoλ
+                else
+                    output_up_first = output_up_curr
+                end
+            else
+                x[indstart_up[j_up]] = output_up_curr
+            end
+        end
+    end
+
+    @inbounds begin
+        i = N
+        yi = T(y[i])
+        if yi + λ <= output_low_curr
+            while jseg < j_low
+                jseg += 1
+                indjseg2 = indstart_low[jseg]
+                while indjseg < indjseg2
+                    x[indjseg] = output_low_first
+                    indjseg += 1
+                end
+                output_low_first = x[indjseg]
+            end
+            while indjseg < i
+                x[indjseg] = output_low_first
+                indjseg += 1
+            end
+            x[indjseg] = yi + λ
+        elseif yi - λ >= output_up_curr
+            while jseg < j_up
+                jseg += 1
+                indjseg2 = indstart_up[jseg]
+                while indjseg < indjseg2
+                    x[indjseg] = output_up_first
+                    indjseg += 1
+                end
+                output_up_first = x[indjseg]
+            end
+            while indjseg < i
+                x[indjseg] = output_up_first
+                indjseg += 1
+            end
+            x[indjseg] = yi - λ
+        else
+            output_low_curr += (yi + λ - output_low_curr) / (i - indstart_low[j_low] + 1)
+            x[indjseg] = output_low_first
+            while j_low > jseg
+                ind = indstart_low[j_low - 1]
+                if output_low_curr < x[ind]
+                    break
+                end
+                output_low_curr += (x[ind] - output_low_curr) *
+                                   ((indstart_low[j_low] - ind) / (i - ind + 1))
+                j_low -= 1
+            end
+            if j_low == jseg
+                if output_up_first >= output_low_curr
+                    while indjseg <= i
+                        x[indjseg] = output_low_curr
+                        indjseg += 1
+                    end
+                else
+                    output_up_curr += (yi - λ - output_up_curr) / (i - indstart_up[j_up] + 1)
+                    x[indjseg] = output_up_first
+                    while j_up > jseg
+                        ind = indstart_up[j_up - 1]
+                        if output_up_curr > x[ind]
+                            break
+                        end
+                        output_up_curr += (x[ind] - output_up_curr) *
+                                          ((indstart_up[j_up] - ind) / (i - ind + 1))
+                        j_up -= 1
+                    end
+                    while jseg < j_up
+                        jseg += 1
+                        indjseg2 = indstart_up[jseg]
+                        while indjseg < indjseg2
+                            x[indjseg] = output_up_first
+                            indjseg += 1
+                        end
+                        output_up_first = x[indjseg]
+                    end
+                    indjseg = indstart_up[j_up]
+                    while indjseg <= i
+                        x[indjseg] = output_up_curr
+                        indjseg += 1
+                    end
+                end
+            else
+                while jseg < j_low
+                    jseg += 1
+                    indjseg2 = indstart_low[jseg]
+                    while indjseg < indjseg2
+                        x[indjseg] = output_low_first
+                        indjseg += 1
+                    end
+                    output_low_first = x[indjseg]
+                end
+                indjseg = indstart_low[j_low]
+                while indjseg <= i
+                    x[indjseg] = output_low_curr
+                    indjseg += 1
+                end
+            end
+        end
+    end
+    return x
 end
